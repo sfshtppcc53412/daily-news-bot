c9247d358fcdcf9686a9b1262bc319647fc0c151
Marti Johnson <140757321+sfshtppcc53412@users.noreply.github.com>
2024-01-17 08:40:12 +0800

    main.py

--- main.py
+++ main.py
@@ -3,26 +3,24 @@ from requests import post
 from os import environ
 
 
-def main():
-    # get pushplus token
-    token = environ.get('token')
+import requests
+from urllib.parse import urljoin
 
+
+def main():
+
+    token = environ.get('token')
     if not token:
-        # 无token则拦截推送
-        return 'PushPlus: 未配置token，无法进行消息推送。'
-    url = 'http://www.pushplus.plus/send/'
-    headers = {'Content-Type':'application/json'}
-	
-	# api_list: [https://api.03c3.cn/zb, https://api.vvhan.com/api/60s, https://api.emoao.com/api/60s]
-    data = {
-        "token": token,
-        "title": "每日新闻推送 bot",
-        "content": "<img src='https://api.03c3.cn/zb' /><img src='https://api.emoao.com/api/60s' />",
-        "template": "html",
-        "channel": "wechat"
+        return 'AnPush: 未配置token，无法进行消息推送。'
+
+    url = urljoin("https://api.anpush.com/push/", token)
+
+    payload = 'title=每日新闻推送 bot&content=your_content&channel=57009'
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
     }
-    data = dumps(data).encode(encoding='utf-8')
-    rsp = post(url=url, data=data, headers=headers)
+
+    response = requests.request("POST", url, headers=headers, data=payload)
 
 
 if __name__ == '__main__':
